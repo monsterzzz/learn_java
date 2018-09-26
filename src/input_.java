b68ac96e8233cfa4d523563dd6cea9fc6edd6a53
monster <373016896@qq.com>
2018-09-27 00:48:08 +0800

    input_learn

---
+++ src/input_.java
@@ -0,0 +1,9 @@
+public class input_ {
+    public static void  main(String[] args){
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        System.out.println("please input your age:");
+        int age = s.nextInt();
+        //int age = 10;
+        System.out.println("your age is " + age);
+    }
+}
